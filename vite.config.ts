254d6c80e3907f6655da2e58837d31b442aa7fbc
Gabriel-Cara <gabricar28@gmail.com>
2026-01-08 14:59:08 -0300

    new test for pwa icons

--- vite.config.ts
+++ vite.config.ts
@@ -30,9 +30,13 @@ export default defineConfig({
         background_color: '#0f172a',
         theme_color: '#0f172a',
         icons: [
-          { src: '/pwa-192x192.png', sizes: '192x192', type: 'image/png' },
-          { src: '/pwa-512x512.png', sizes: '512x512', type: 'image/png' },
-          { src: '/maskable-512x512.png', sizes: '512x512', type: 'image/png', purpose: 'any maskable' },
+          { src: "/icons/icon-192x192.png", sizes: "192x192", type: "image/png", purpose: "any" },
+          { src: "/icons/icon-512x512.png", sizes: "512x512", type: "image/png", purpose: "any" },
+
+          { src: "/icons/maskable-192x192.png", sizes: "192x192", type: "image/png", purpose: "maskable" },
+          { src: "/icons/maskable-512x512.png", sizes: "512x512", type: "image/png", purpose: "maskable" },
+
+          { src: "/icons/icon-180x180.png", sizes: "180x180", type: "image/png", purpose: "any" }
         ],
       },
       workbox: {
